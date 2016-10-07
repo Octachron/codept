92a2a95510236a33a54fb5bdc8edc13e66a414bf
octachron <octa@polychoron.fr>
2016-10-07 23:08:44 +0200

    Improved main

--- src/main.ml
+++ src/main.ml
@@ -1,54 +1,41 @@
+module Cmd = Arg
 open M2l
 
 module S = Module.Sig
 let std = Format.std_formatter
 
-module File() = struct
-  let lex_test = Lexing.from_channel @@ open_in Sys.argv.(1)
 
-  let ast = Parse.implementation lex_test
+let classify f =
+  if Filename.check_suffix f ".mli" then
+    Unit.Signature
+  else
+    Unit.Structure
 
-  let () =
-    let start =  Ast_analyzer.structure ast in
-    Pp.fp std "M2l: %a@." M2l.pp start;
-    start
-    |> Compute.basic
-    |> Normalize.all
-    |> snd
-    |> Pp.fp std  "Basic:\n %a@." M2l.pp;
-    match start |> Compute.Sg.m2l S.empty with
-    | Done (_state,d) -> Pp.fp std "Computation finished:\n %a@." S.pp d
-    | Halted h -> Pp.fp std "Computation halted at:\n %a@." M2l.pp h
-
-end
-
-module Deps() = struct
-  let classify f =
-    if Filename.check_suffix f ".mli" then
-      Unit.Signature
-    else
-      Unit.Structure
-
-  let files = match Array.to_list Sys.argv with
-    | [] -> assert false
-    |  _ :: q -> q
-
-  let names = List.map Unit.extract_name files
-
-  let units = List.map (fun f->
-      Unit.read_file (classify f) f) files
+let file f =
+  let lex_test = Lexing.from_channel @@ open_in f in
+  let ast = Parse.implementation lex_test in
+  let start =  Ast_analyzer.structure ast in
+  Pp.fp std "M2l: %a@." M2l.pp start;
+  start
+  |> Compute.basic
+  |> Normalize.all
+  |> snd
+  |> Pp.fp std  "Basic:\n %a@." M2l.pp;
+  match start |> Compute.Sg.m2l S.empty with
+  | Done (_state,d) -> Pp.fp std "Computation finished:\n %a@." S.pp d
+  | Halted h -> Pp.fp std "Computation halted at:\n %a@." M2l.pp h
 
 
-  let () = List.iter (Unit.pp std) units;
-    Pp.p "***\n***\n***\n"
+(*
+let files = match Array.to_list Sys.argv with
+  | [] -> assert false
+  |  _ :: q -> q
+*)
 
-  module Envt = Compute.Tracing
-  let core = Envt.start @@ Name.Set.of_list names
-  let units = Unit.resolve_dependencies core units
-
+(*
   let order =
     let compute (i,m) (u:Unit.t) = i+1, Name.Map.add u.name i m in
-    snd @@ List.fold_left compute (0,Name.Map.empty) @@ List.rev units
+    snd @@ List.fold_left compute (0,Name.Map.empty) @@ List.rev_map units
 
   let compare order x y =
     let get x=Name.Map.find_opt x order in
@@ -57,32 +44,17 @@ module Deps() = struct
     | None, Some _ -> -1
     | Some _, None -> 1
     | None, None -> compare x y
+*)
 
-  let () = List.iter (Unit.pp std) units
 
-end
-
-module Modules() = struct
-  let classify f =
-    if Filename.check_suffix f ".mli" then
-      Unit.Signature
-    else
-      Unit.Structure
-
-  let files = match Array.to_list Sys.argv with
+(*  let files = match Array.to_list Sys.argv with
     | [] -> assert false
     |  _ :: q -> q
-
-  let names = List.map Unit.extract_name files
-
-  let units = List.map (fun f->
-      Unit.read_file (classify f) f) files
-
-  module Envt = Compute.Tracing
-  let core = Envt.start @@ Name.Set.of_list names
-  let units = Unit.resolve_dependencies core units
+*)
 
 
+
+  (*
   let order =
     let compute (i,m) u = i+1, Name.Map.add u.Unit.name i m in
     snd @@ List.fold_left compute (0,Name.Map.empty) @@ List.rev units
@@ -94,17 +66,63 @@ module Modules() = struct
     | None, Some _ -> -1
     | Some _, None -> 1
     | None, None -> compare x y
+*)
 
-  let pp_module ppf u =
-    let open Unit in
-    Pp.fp ppf "%a: %a\n" Pp.(tlist ~sep:"/" string) u.path.file
-      Pp.(tlist ~sep:" " string)
-      ((*List.sort (compare order) @@*) Name.Set.elements u.dependencies)
+let deps files =
+  let names = List.map Unit.extract_name files in
+  let units = Unit.( split @@ group_by classify files ) in
+  let () =
+    List.iter (Unit.pp std) units.mli;
+    List.iter (Unit.pp std) units.ml;
+    Pp.p "***\n***\n***\n"
+  in
+  let module Envt = Compute.Tracing in
+  let core = Envt.start @@ Name.Set.of_list names in
+  let {Unit.ml; mli} = Unit.resolve_split_dependencies core units in
+  List.iter (Unit.pp std) mli;
+  List.iter (Unit.pp std) ml
 
-  let () = List.iter (pp_module std)
-      (List.sort Unit.(fun x y -> compare x.path.file y.path.file) units)
+let pp_module ppf u =
+  let open Unit in
+  Pp.fp ppf "%a: %a\n" Pp.(tlist ~sep:"/" string) u.path.file
+    Pp.(tlist ~sep:" " string)
+    ((*List.sort (compare order) @@*) Name.Set.elements u.dependencies)
 
-end
+let modules files =
+  let names = List.map Unit.extract_name files in
+  let units = Unit.( split @@ group_by classify files ) in
+  let module Envt = Compute.Tracing in
+  let core = Envt.start @@ Name.Set.of_list names in
+  let {Unit.ml; mli} = Unit.resolve_split_dependencies core units in
+  let print units = List.iter (pp_module std)
+      (List.sort Unit.(fun x y -> compare x.path.file y.path.file) units) in
+    print mli; print ml
 
 
-module R = Modules()
+let usage_msg = "fdep is an alternative dependencies solver for OCaml"
+
+let extract_files () =
+  let current = 1 + !(Cmd.current) in
+  let rec files_from k = if k = Array.length Sys.argv then []
+    else Sys.argv.(k) :: files_from (k+1) in
+  files_from current
+
+
+let cmd_module () =
+  modules @@ extract_files ()
+
+let cmd_deps () =
+  deps @@ extract_files ()
+
+let cmd_file () =
+  file Sys.argv.(1 + !Cmd.current)
+
+let anon_fun _name = ()
+
+let args =
+    Cmd.["-modules", Unit cmd_module, "print raw modules dependencies";
+     "-deps", Unit cmd_deps, "print detailed dependencies";
+     "-file", Unit cmd_file, "print simple analysis of one file"
+    ]
+
+let () = Cmd.parse args anon_fun usage_msg
