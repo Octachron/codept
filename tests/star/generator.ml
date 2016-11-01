955f8bece503ff3f287236e3b3727f5827a07391
octachron <octa@polychoron.fr>
2016-11-01 23:59:16 +0100

    Star dependencies test

---
+++ tests/star/generator.ml
@@ -0,0 +1,22 @@
+
+let name = Format.sprintf "M%03d"
+let filename = Format.sprintf "m%03d.mli"
+
+let make n n_max =
+  let f = open_out @@ filename n in
+  let fp = Format.fprintf (Format.formatter_of_out_channel f) in
+  if n < n_max then
+    for i = n_max downto 0 do
+      fp "open %s\n" (name i)
+    done;
+  for i = 1 to n do
+    fp "module %s: sig end\n" (name i)
+  done;
+  close_out f
+
+let n = int_of_string @@ Sys.argv.(1)
+
+let () =
+  for i = 1 to n do
+    make i n
+  done;
