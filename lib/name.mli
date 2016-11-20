59eebba1e8b956a48a7e5c1dc791d473a12a5650
octachron <octa@polychoron.fr>
2016-11-20 01:29:51 +0100

    Name.mli

---
+++ lib/name.mli
@@ -0,0 +1,17 @@
+type t = string
+type name = t
+val pp: Format.formatter -> t -> unit
+
+module Set: sig
+  include Set.S with type elt = t
+  val pp: Format.formatter -> t -> unit
+end
+type set = Set.t
+
+module Map: sig
+  include Map.S with type key = t
+  val find_opt: key -> 'a t -> 'a option
+  val union': 'a t -> 'a t -> 'a t
+end
+
+type 'a map = 'a Map.t
