ddfa5081384a5552baff2c1503a79ab0c0b34c3d
octachron <octa@polychoron.fr>
2017-01-09 21:19:44 +0100

    Single.mli

---
+++ full/single.mli
@@ -0,0 +1,16 @@
+(** Analysis on single files *)
+
+type single = Format.formatter -> Params.t -> string * string -> unit
+
+(** Use the heuristic approximate parser and display the resulting
+    lower and upper bound m2l approximation *)
+val approx_file: single
+
+(** Do a single pass of the interpreter and display the result *)
+val one_pass: single
+
+(** Display an human-readable representation of the m2l ast *)
+val m2l: single
+
+(** Display a machine-readable s-expression for the m2l ast *)
+val m2l_sexp: single
