a00ba3d7a1bfa87313168f7a7764da670e237693
octachron <octa@polychoron.fr>
2017-01-12 22:56:36 +0100

    Analysis.mli

---
+++ full/analysis.mli
@@ -0,0 +1,18 @@
+(** Analysys glue code *)
+
+type param = {
+  transparent_aliases: bool;
+  transparent_extension_nodes: bool;
+  polycy: Fault.Polycy.t;
+  no_stdlib:bool;
+  std_otherlibs:bool;
+  closed_world: bool;
+  sig_only:bool;
+}
+
+(** Lift parameter to a module parameter *)
+val lift: param -> (module Interpreter.param)
+
+(** [main param task] performs dependency analysis
+    with parameters [param] on the given task *)
+val main: param -> Common.task -> Unit.r list Unit.pair
