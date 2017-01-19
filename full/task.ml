c3d5fc129ea9bb16d4e1578f215f33674aa0fc77
octachron <octa@polychoron.fr>
2017-01-19 20:32:34 +0100

    Do not deactivate completely unknow file fault

--- full/task.ml
+++ full/task.ml
@@ -57,31 +57,32 @@ let add_sig task ssig =
   @@ parse_sig
   @@ Lexing.from_string ssig
 
-let rec add_file ~prefix ~cycle_guard param task name0 =
+let rec add_file ~prefix ~cycle_guard ~polycy param task name0 =
   let name = String.concat "/" (prefix @ [name0]) in
-  let open L in
-  let polycy = let open Fault in
-    Polycy.set_err (Codept_polycy.unknown_extension, Level.whisper)
-      !param.[polycy] in
   if Sys.file_exists name then
-    match classify polycy !param.[synonyms] name with
+    match classify polycy L.(!param.[synonyms]) name with
     | None -> if Sys.is_directory name then
-        add_dir ~prefix ~cycle_guard param task ~dir_name:name0 ~abs_name:name
+        let polycy = let open Fault in
+          Polycy.set_err (Codept_polycy.unknown_extension, Level.whisper)
+            L.(!param.[polycy]) in
+        add_dir ~prefix ~polycy ~cycle_guard param task
+          ~dir_name:name0 ~abs_name:name
     | Some { kind = Implementation; format } ->
       add_impl format task name
     | Some { kind = Interface; format } -> add_intf format task name
     | Some { kind = Signature; _ } -> add_sig0 task @@ read_sigfile name
-and add_dir ~prefix ~cycle_guard param task ~dir_name ~abs_name =
+and add_dir ~polycy ~prefix ~cycle_guard param task ~dir_name ~abs_name =
     if  cycle_guard && dir_name = "." then
        ()
     else
       let cycle_guard = dir_name = "." in
       let files = Sys.readdir abs_name in
       Array.iter
-        (add_file ~prefix:(dir_name::prefix) ~cycle_guard param task)
+        (add_file ~polycy ~prefix:(dir_name::prefix) ~cycle_guard param task)
         files
 
-let add_file = add_file ~cycle_guard:false ~prefix:[]
+let add_file param = add_file ~polycy:L.(!param.[polycy])
+    ~cycle_guard:false ~prefix:[] param
 
 let add_invisible_file param task name =
   if Sys.file_exists name then
