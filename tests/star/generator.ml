dbdac0d32869ac27927a2d374fbf6e37522735e3
octachron <octa@polychoron.fr>
2017-01-23 02:08:28 +0100

    Aesthetic modification of generated code

--- tests/star/generator.ml
+++ tests/star/generator.ml
@@ -4,13 +4,15 @@ let filename = Format.sprintf "m%03d.mli"
 
 let make n n_max =
   let f = open_out @@ filename n in
-  let fp = Format.fprintf (Format.formatter_of_out_channel f) in
+  let out = Format.formatter_of_out_channel f in
+  let fp x = Format.fprintf out x in
   if n < n_max then
     for i = n_max downto 1 do
-      fp "open %s\n" (name i)
+      fp "open %s " (name i);
+      if i mod 6 = 5 then fp "\n"
     done;
   for i = 1 to n do
-    fp "module %s: sig end\n" (name i)
+    fp "module %s: sig end\n%!" (name i)
   done;
   close_out f
 
