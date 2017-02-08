1d3d57b6bacfcae8d29bfa1c7e25a13b59b6e777
octachron <octa@polychoron.fr>
2017-02-08 15:23:37 +0100

    Commute ε annotation in m2l pretty output

--- lib/m2l.ml
+++ lib/m2l.ml
@@ -626,7 +626,7 @@ and pp_annot ppf {access;values; packed} =
 and pp_access ppf s =  if Name.Map.cardinal s = 0 then () else
     Pp.fp ppf "access:@[<hv>{%a}@]" (Pp.list pp_access_elt) (Name.Map.bindings s)
 and pp_access_elt ppf (name,edge) =
-  Pp.fp ppf "%s%s" name (if edge = Deps.Edge.Normal then "" else "∙ε")
+  Pp.fp ppf "%s%s" (if edge = Deps.Edge.Normal then "" else "ε∙") name
 and pp_opaque ppf me = Pp.fp ppf "⟨%a⟩" pp_me me
 and pp_bind ppf {name;expr} =
   match expr with
