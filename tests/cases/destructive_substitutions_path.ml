369132730f62672983f3e3d1edbaf2a03091d522
octachron <octa@polychoron.fr>
2018-01-14 20:22:13 +0100

    Updated destructive substitution test

--- tests/cases/destructive_substitutions_path.ml
+++ tests/cases/destructive_substitutions_path.ml
@@ -1,6 +1,6 @@
 module type s = sig
   module Ext: sig end
-  module Middle:sig module Ext2: sig end end
+  module Middle:sig module Deep_inner: sig end module Ext2: sig end end
   module Inner:sig end
 end
 
@@ -9,13 +9,14 @@ module Void = struct end
 module F(): s with module Ext := Void and module Middle.Ext2 := Void =
 struct
   module Inner = struct end
-  module Middle = struct end
+  module Middle = struct module Deep_inner = struct end end
 end
 
 module M = F()
 open M
 open Inner
 open Middle
+open Deep_inner
 
 module A = struct open Ext end
 module B = struct open Ext2 end
