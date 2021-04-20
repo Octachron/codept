ef31a9697ff27446beea23bac26a96afb4cbd4bf
octachron <octa@polychoron.fr>
2021-04-20 22:09:13 +0200

    dep_zipper.mli

---
+++ lib/dep_zipper.mli
@@ -0,0 +1,7 @@
+module Outline(Env : Stage.envt):
+ Stage.generic_outliner with
+  type envt := Env.t and type final := Deps.t
+  and type 'a with_param := 'a Stage.param
+
+module Make(Env:Stage.envt)(Param:Stage.param):
+  Stage.outliner with type envt := Env.t 
