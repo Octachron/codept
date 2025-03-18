62dfec6eb76527eee01ba242860233032aa102bb
octachron <octa@polychoron.fr>
2025-03-18 20:39:48 +0100

    clean-up preprocessor

--- preprocessor.ml
+++ preprocessor.ml
@@ -1,11 +1,7 @@
-exception Parse_error of string
-
-type v = {major:int; minor:int}
+type v = {major:int; minor:int} [@@warning "-69"]
 type version =
   | Finite of v
   | Inf
-
-let parse_error fmt = Format.ksprintf (fun s ->  raise (Parse_error s)) fmt
 
 type 'a parse = {pos:int; x:'a}
 let scan {pos; x} = if pos < String.length x then Some x.[pos] else None
@@ -13,8 +9,6 @@ let incr {pos; x} = {pos=succ pos; x}
 
 let rec skip c s =
   if scan s = Some c then skip c (incr s) else s
-let skip_dot = skip '.'
-
 
 let parse_integer =
   let rec parse_integer rem s = match scan s with
@@ -61,33 +55,6 @@ let parse x =
     let x = List.fold_left (fun x c -> skip c x) x [' '; ','; ' '] in
     let right, _ = parse_version x in
     Start {left; right}
-
-(*
-let rec parse s = match scan s with
-  | Some ' ' -> parse (incr s)
-  | Some '>' -> parse_mixt Greater (incr s)
-  | Some '<' -> parse_mixt Lesser (incr s)
-  | Some '=' -> with_version Equal (incr s)
-  | Some c when c = ge.[0] -> parse_unicode (incr s)
-  | Some '\n' -> parse_error "Unexpected end of input in %s" s.x
-  | Some 'e' -> if sub s "end" then End else parse_error "Unexpected character in %s" s.x
-  | Some _ -> parse_error "Unexpected character in %s" s.x
-  | None -> parse_error "Unexpected end"
-and parse_mixt base s =
-  if scan s = Some '=' then  with_version (Or(Equal,base)) (incr s)
-  else with_version base s
-and with_version c s = Start {c; version=parse_version s }
-and parse_unicode s =
-  if not (scan s=Some ge.[1]) then parse_error "Unexpected character in %s" s.x
-  else
-    let s = incr s in
-    let base =
-      let c = scan s in
-      if c = Some ge.[2] then Greater
-      else if c = Some le.[2] then Lesser
-      else parse_error "Unexpected character in %s" s.x in
-    with_version (Or(Equal,base)) (incr s)
-*)
 
 let conditional line =
   if String.length line > 0 && line.[0] = '#'
